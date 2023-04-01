971b85e93fb33027965b7f98d950c944d3d00085
TomZeta7 <129407088+tomgato242@users.noreply.github.com>
2023-04-01 11:49:24 -0400

    class1.cloud

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,6 +1,6 @@
 import pygame
 
-from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
+from dino_runner.utils.constants import BG, CLOUD, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS, ICON
 
 
 class Game:
@@ -8,12 +8,15 @@ class Game:
         pygame.init()
         pygame.display.set_caption(TITLE)
         pygame.display.set_icon(ICON)
-        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))#Ancho y alto
         self.clock = pygame.time.Clock()
         self.playing = False
         self.game_speed = 20
         self.x_pos_bg = 0
-        self.y_pos_bg = 380
+        self.y_pos_bg = 500
+        self.posX_cloud = 2000
+        self.PosY_cloud = 200
+        
 
     def run(self):
         # Game loop: events - update - draw
@@ -31,11 +34,21 @@ class Game:
 
     def update(self):
         pass
+    
+    def cloud(self):
+        imagen= CLOUD.get_width()
+        self.screen.blit(CLOUD, (imagen + self.posX_cloud, self.PosY_cloud))
+        if self.posX_cloud <= -imagen:
+            self.screen.blit(CLOUD, (imagen + self.posX_cloud, self.PosY_cloud))
+            self.posX_cloud = 2000
+        self.posX_cloud -= self.game_speed
+        
 
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
         self.draw_background()
+        self.cloud()
         pygame.display.update()
         pygame.display.flip()
 
